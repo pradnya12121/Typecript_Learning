29b580211c616dd761531aca91c75babfcec9783
pradnya12121 <pradnya.kolase1994@gmail.com>
2022-11-08 10:06:35 +0530

    first typescript program variables
    first typescript program variables

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+var n1 = 10;
+var n2 = 20;
+var n3 = n1 + n2;
+console.log(n3);
+var s1 = "You are creater of destiny";
+var s2 = "Powerful people come from powerful places";
+console.log("string one :".concat(s1));
+console.log("string two : ".concat(s2));
---
+++ 01_variables.ts
@@ -0,0 +1,10 @@
+var n1: number =10;
+var n2: number=20;
+var n3:number=n1+n2;
+console.log(n3);
+
+var s1: string = "You are creater of destiny";
+var s2: string = "Powerful people come from powerful places";
+
+console.log(`string one :${s1}`);
+console.log(`string two : ${s2}`);
